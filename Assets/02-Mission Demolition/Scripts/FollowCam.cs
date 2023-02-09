f7b53422dd71be0645f571bd725ad431af6cec82
kylematchett <113548697+kylematchett@users.noreply.github.com>
2023-02-08 19:38:38 -0500

    camera following projectile

---
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+    static public GameObject POI; // The static point of interest // a
+
+    [Header("Inscribed")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+    [Header("Dynamic")]
+    public float camZ; // The desired Z pos of the camera
+
+    void Awake() {
+        camZ = this.transform.position.z;
+    }
+    void FixedUpdate () {
+        Vector3 destination = Vector3.zero;
+        if ( POI != null ) {
+            // c
+            // If the POI has a Rigidbody, check to see if it is sleeping
+            Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+            if ( ( poiRigid != null ) && poiRigid.IsSleeping() ) { // d
+                POI = null;
+            }
+        }
+        
+        if ( POI != null ) {
+            // e
+            destination = POI.transform.position;
+        }
+        destination.x = Mathf.Max( minXY.x, destination.x );
+        destination.y = Mathf.Max( minXY.y, destination.y );
+
+        destination = Vector3.Lerp(transform.position, destination, easing);
+
+        // Force destination.z to be camZ to keep the camera far enough away
+        destination.z = camZ;
+        // Set the camera to the destination
+        transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
+    }
+}
